c7ea93a663ae4fe7243bcef56edf4a3d2b9ae67b
yaliun <yaliun@gmail.com>
2017-01-23 17:36:31 +0900

    test branch에서 쉊ㅇ 및 commit

--- chap01/src/sec06/exam01/Hello.java
+++ chap01/src/sec06/exam01/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, welcome to the java world!");
 		System.out.println("1 st commit");
+		System.out.println("test branch edit");
 	}
 }
